242f2006bf3819ef3621695583d63df394f6cb2d
Joe Pea <joe@trusktr.io>
2019-06-07 23:10:47 -0700

    add Layers description

--- src/as/glas/core/Layers.ts
+++ src/as/glas/core/Layers.ts
@@ -3,6 +3,22 @@
  * @author Joe Pea / https://github.com/trusktr
  */
 
+/**
+ * A [page:Layers] object assigns an [page:Object3D] to 1 or more of 32 layers
+ * numbered 0 to 31 - internally the layers are stored as a [bit
+ * mask](https://en.wikipedia.org/wiki/Mask_(computing)), and by default all
+ * Object3Ds are a member of layer 0.<br /><br />
+ *
+ * This can be used to control visibility - an object must share a layer with
+ * a [page:Camera camera] to be visible when that camera's view is
+ * renderered.<br /><br />
+ *
+ * All classes that inherit from [page:Object3D] have an
+ * [page:Object3D.layers] property which is an instance of this class.
+ *
+ * Examples:
+ * - [example:webgl_layers WebGL / layers]
+ */
 export class Layers {
 	/**
 	 * A bit mask storing which of the 32 layers this layer's object is currently
