f9e4800e5fc0713e0177dd64638b3aeda43c7926
corruptedzulu <corruptedzulu@gmail.com>
2019-06-09 14:01:13 -0700

    BoxGeometry no longer has red underlines

--- src/as/glas/geometries/BoxGeometry.ts
+++ src/as/glas/geometries/BoxGeometry.ts
@@ -14,18 +14,7 @@ import { Vector3 } from '../math/Vector3.js';
 // Extras / Geometries /////////////////////////////////////////////////////////////////////
 export class BoxBufferGeometry extends BufferGeometry {
 
-	constructor(
-		width?: number,
-		height?: number,
-		depth?: number,
-		widthSegments?: number,
-		heightSegments?: number,
-		depthSegments?: number
-	) {
-        super();
-    }
-
-	parameters: {
+    parameters: {
 		width: number;
 		height: number;
 		depth: number;
@@ -33,6 +22,160 @@ export class BoxBufferGeometry extends BufferGeometry {
 		heightSegments: number;
 		depthSegments: number;
 	};
+
+
+    indices: number[] = [];
+	vertices: number[] = [];
+	normals: number[] = [];
+	uvs: number[] = [];
+
+	// helper variables
+
+	numberOfVertices = 0;
+    groupStart = 0;
+    
+    scope = this;
+
+
+	constructor(
+		width: number = 1,
+		height: number = 1,
+		depth: number = 1,
+		widthSegments: number = 1,
+		heightSegments: number = 1,
+		depthSegments: number = 1
+	) {
+        super();
+
+        this.buildPlane( 'z', 'y', 'x', - 1, - 1, depth, height, width, depthSegments, heightSegments, 0 ); // px
+        this.buildPlane( 'z', 'y', 'x', 1, - 1, depth, height, - width, depthSegments, heightSegments, 1 ); // nx
+        this.buildPlane( 'x', 'z', 'y', 1, 1, width, depth, height, widthSegments, depthSegments, 2 ); // py
+        this.buildPlane( 'x', 'z', 'y', 1, - 1, width, depth, - height, widthSegments, depthSegments, 3 ); // ny
+        this.buildPlane( 'x', 'y', 'z', 1, - 1, width, height, depth, widthSegments, heightSegments, 4 ); // pz
+        this.buildPlane( 'x', 'y', 'z', - 1, - 1, width, height, - depth, widthSegments, heightSegments, 5 ); // nz
+
+        // build geometry
+
+        this.setIndex( this.indices );
+        this.addAttribute( 'position', new Float32BufferAttribute( this.vertices, 3 ) );
+        this.addAttribute( 'normal', new Float32BufferAttribute( this.normals, 3 ) );
+        this.addAttribute( 'uv', new Float32BufferAttribute( this.uvs, 2 ) );
+    }
+
+	
+
+    buildPlane( u: string, v: string, w: string, udir: i64, vdir: i64, width: i64, height: i64, depth: i64, gridX: i64, gridY: i64, materialIndex: i64 ) {
+
+		var segmentWidth = width / gridX;
+		var segmentHeight = height / gridY;
+
+		var widthHalf = width / 2;
+		var heightHalf = height / 2;
+		var depthHalf = depth / 2;
+
+		var gridX1 = gridX + 1;
+		var gridY1 = gridY + 1;
+
+		var vertexCounter = 0;
+		var groupCount = 0;
+
+		var ix, iy;
+
+		var vector = new Vector3();
+
+		// generate vertices, normals and uvs
+
+		for ( iy = 0; iy < gridY1; iy ++ ) {
+
+			var y = iy * segmentHeight - heightHalf;
+
+			for ( ix = 0; ix < gridX1; ix ++ ) {
+
+				var x = ix * segmentWidth - widthHalf;
+
+				// set values to correct vector component
+
+				vector.setX( x * udir );
+				vector.setY( y * vdir );
+				vector.setZ( depthHalf );
+
+				// now apply vector to vertex buffer
+
+                this.vertices.push( vector.x );
+                this.vertices.push( vector.y );
+                this.vertices.push( vector.z );
+
+				// set values to correct vector component
+
+				vector.setX( 0 );
+                vector.setY( 0 );
+                vector.setZ( depth > 0 ? 1 : - 1 );
+
+				// now apply vector to normal buffer
+
+                this.normals.push( vector.x );
+                this.normals.push( vector.y );
+                this.normals.push( vector.z );
+
+
+				// uvs
+
+				this.uvs.push( ix / gridX );
+				this.uvs.push( 1 - ( iy / gridY ) );
+
+				// counters
+
+				vertexCounter += 1;
+
+			}
+
+		}
+
+		// indices
+
+		// 1. you need three indices to draw a single face
+		// 2. a single segment consists of two faces
+		// 3. so we need to generate six (2*3) indices per segment
+
+		for ( iy = 0; iy < gridY; iy ++ ) {
+
+			for ( ix = 0; ix < gridX; ix ++ ) {
+
+				var a = this.numberOfVertices + ix + gridX1 * iy;
+				var b = this.numberOfVertices + ix + gridX1 * ( iy + 1 );
+				var c = this.numberOfVertices + ( ix + 1 ) + gridX1 * ( iy + 1 );
+				var d = this.numberOfVertices + ( ix + 1 ) + gridX1 * iy;
+
+				// faces
+
+                this.indices.push( a );
+                this.indices.push( b );
+                this.indices.push( d );
+                this.indices.push( b );
+                this.indices.push( c );
+                this.indices.push( d );
+
+				// increase counter
+
+				groupCount += 6;
+
+			}
+
+		}
+
+		// add a group to the geometry. this will ensure multi material support
+
+		this.scope.addGroup( this.groupStart, groupCount, materialIndex );
+
+		// calculate new start value for groups
+
+		this.groupStart += groupCount;
+
+		// update total number of vertices
+
+		this.numberOfVertices += vertexCounter;
+
+	}
 
 }
 
@@ -57,20 +200,17 @@ export class BoxGeometry extends Geometry {
 	 * @param depthSegments — Number of segmented faces along the depth of the sides.
 	 */
 	constructor(
-		width: number,
-		height: number,
-		depth: number,
-		widthSegments: number,
-		heightSegments: number,
-		depthSegments: number
+		width: number = 1,
+		height: number = 1,
+		depth: number = 1,
+		widthSegments: number = 1,
+		heightSegments: number = 1,
+		depthSegments: number = 1
 	) {
         super();
-        this.width = width = 1;
-        this.height = height = 1;
-        this.depth = depth = 1;
-        this.widthSegments = widthSegments = 1;
-        this.heightSegments = heightSegments = 1;
-        this.depthSegments = depthSegments = 1;
+
+        this.fromBufferGeometry( new BoxBufferGeometry( width, height, depth, widthSegments, heightSegments, depthSegments ) );
+	    this.mergeVertices();
     }
 
 
