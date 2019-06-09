31c7c133c736ac08a94345d679f291271159b324
Joe Pea <joe@trusktr.io>
2019-06-09 14:23:55 -0700

    formatted and added a missing type to Camera.d.ts

--- src/as/glas/cameras/Camera.d.ts
+++ src/as/glas/cameras/Camera.d.ts
@@ -1,6 +1,6 @@
-import { Matrix4 } from './../math/Matrix4';
-import { Vector3 } from './../math/Vector3';
-import { Object3D } from './../core/Object3D';
+import {Matrix4} from './../math/Matrix4'
+import {Vector3} from './../math/Vector3'
+import {Object3D} from './../core/Object3D'
 
 // Cameras ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -8,28 +8,27 @@ import { Object3D } from './../core/Object3D';
  * Abstract base class for cameras. This class should always be inherited when you build a new camera.
  */
 export class Camera extends Object3D {
+	/**
+	 * This constructor sets following properties to the correct type: matrixWorldInverse, projectionMatrix and projectionMatrixInverse.
+	 */
+	constructor()
 
 	/**
-   * This constructor sets following properties to the correct type: matrixWorldInverse, projectionMatrix and projectionMatrixInverse.
-   */
-	constructor();
+	 * This is the inverse of matrixWorld. MatrixWorld contains the Matrix which has the world transform of the Camera.
+	 */
+	matrixWorldInverse: Matrix4
 
 	/**
-   * This is the inverse of matrixWorld. MatrixWorld contains the Matrix which has the world transform of the Camera.
-   */
-	matrixWorldInverse: Matrix4;
+	 * This is the matrix which contains the projection.
+	 */
+	projectionMatrix: Matrix4
+	projectionMatrixInverse: Matrix4
 
-	/**
-   * This is the matrix which contains the projection.
-   */
-	projectionMatrix: Matrix4;
+	isCamera: true
 
-	isCamera: true;
+	copy(source: Camera, recursive?: boolean): this
 
-	copy( source: Camera, recursive?: boolean ): this;
+	getWorldDirection(target: Vector3): Vector3
 
-	getWorldDirection( target: Vector3 ): Vector3;
-
-	updateMatrixWorld( force?: boolean ): void;
-
+	updateMatrixWorld(force?: boolean): void
 }
