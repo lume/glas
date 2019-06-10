e10a39ca6c6d1e76cdec7f1d486c3e33477498be
Joe Pea <joe@trusktr.io>
2019-06-10 01:19:04 -0700

    remove unused comments

--- src/as/glas/core/Layers.spec.ts
+++ src/as/glas/core/Layers.spec.ts
@@ -28,22 +28,18 @@ describe('Layers', () => {
 
 			a.set(0)
 			a.enable(0)
-			// assert.strictEqual(a.mask, 1, 'Enable channel 0 with mask 0')
 			expect<u32>(a.mask).toBe(1)
 
 			a.set(0)
 			a.enable(1)
-			// assert.strictEqual(a.mask, 3, 'Enable channel 1 with mask 0')
 			expect<u32>(a.mask).toBe(3)
 
 			a.set(1)
 			a.enable(0)
-			// assert.strictEqual(a.mask, 3, 'Enable channel 0 with mask 1')
 			expect<u32>(a.mask).toBe(3)
 
 			a.set(1)
 			a.enable(1)
-			// assert.strictEqual(a.mask, 2, 'Enable channel 1 with mask 1')
 			expect<u32>(a.mask).toBe(2)
 		})
 	})
@@ -54,22 +50,18 @@ describe('Layers', () => {
 
 			a.set(0)
 			a.toggle(0)
-			// assert.strictEqual(a.mask, 0, 'Toggle channel 0 with mask 0')
 			expect<u32>(a.mask).toBe(0)
 
 			a.set(0)
 			a.toggle(1)
-			// assert.strictEqual(a.mask, 3, 'Toggle channel 1 with mask 0')
 			expect<u32>(a.mask).toBe(3)
 
 			a.set(1)
 			a.toggle(0)
-			// assert.strictEqual(a.mask, 3, 'Toggle channel 0 with mask 1')
 			expect<u32>(a.mask).toBe(3)
 
 			a.set(1)
 			a.toggle(1)
-			// assert.strictEqual(a.mask, 0, 'Toggle channel 1 with mask 1')
 			expect<u32>(a.mask).toBe(0)
 		})
 	})
@@ -80,22 +72,18 @@ describe('Layers', () => {
 
 			a.set(0)
 			a.disable(0)
-			// assert.strictEqual(a.mask, 0, 'Disable channel 0 with mask 0')
 			expect<u32>(a.mask).toBe(0)
 
 			a.set(0)
 			a.disable(1)
-			// assert.strictEqual(a.mask, 1, 'Disable channel 1 with mask 0')
 			expect<u32>(a.mask).toBe(1)
 
 			a.set(1)
 			a.disable(0)
-			// assert.strictEqual(a.mask, 2, 'Disable channel 0 with mask 1')
 			expect<u32>(a.mask).toBe(2)
 
 			a.set(1)
 			a.disable(1)
-			// assert.strictEqual(a.mask, 0, 'Disable channel 1 with mask 1')
 			expect<u32>(a.mask).toBe(0)
 		})
 	})
@@ -105,15 +93,12 @@ describe('Layers', () => {
 			const a = new Layers()
 			const b = new Layers()
 
-			// assert.ok(a.test(b), 'Start out true')
 			expect<bool>(a.test(b)).toBe(true)
 
 			a.set(1)
-			// assert.notOk(a.test(b), 'Set channel 1 in a and fail the QUnit.test')
 			expect<bool>(a.test(b)).toBe(false)
 
 			b.toggle(1)
-			// assert.ok(a.test(b), 'Toggle channel 1 in b and pass again')
 			expect<bool>(a.test(b)).toBe(true)
 		})
 	})
