ac92724609f44faabcebb8471e4d466ba3ae522a
Joe Pea <joe@trusktr.io>
2019-06-11 20:09:05 -0700

    enable a couple needed methods in Matrix4

--- src/as/glas/math/Matrix4.ts
+++ src/as/glas/math/Matrix4.ts
@@ -36,7 +36,7 @@ import {Matrix} from './Matrix3'
  * m.multiplyMatrices( m1, m2 );
  * m.multiply( m3 );
  */
-export class Matrix4 implements Matrix {
+export class Matrix4 /*implements Matrix*/ {
 	/**
 	 * Array with matrix values.
 	 */
@@ -46,34 +46,34 @@ export class Matrix4 implements Matrix {
 		this.elements = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
 	}
 
-	// // prettier-ignore
-	// /**
-	//  * Sets all fields of this matrix.
-	//  */
-	// set(
-	// 	n11: number, n12: number, n13: number, n14: number,
-	// 	n21: number, n22: number, n23: number, n24: number,
-	// 	n31: number, n32: number, n33: number, n34: number,
-	// 	n41: number, n42: number, n43: number, n44: number
-	// ): Matrix4 {
-	// 	const te = this.elements
+	// prettier-ignore
+	/**
+	 * Sets all fields of this matrix.
+	 */
+	set(
+		n11: number, n12: number, n13: number, n14: number,
+		n21: number, n22: number, n23: number, n24: number,
+		n31: number, n32: number, n33: number, n34: number,
+		n41: number, n42: number, n43: number, n44: number
+	): Matrix4 {
+		const te = this.elements
 
-	// 	te[0] = n11; te[4] = n12; te[8] = n13; te[12] = n14
-	// 	te[1] = n21; te[5] = n22; te[9] = n23; te[13] = n24
-	// 	te[2] = n31; te[6] = n32; te[10] = n33; te[14] = n34
-	// 	te[3] = n41; te[7] = n42; te[11] = n43; te[15] = n44
+		te[0] = n11; te[4] = n12; te[8] = n13; te[12] = n14
+		te[1] = n21; te[5] = n22; te[9] = n23; te[13] = n24
+		te[2] = n31; te[6] = n32; te[10] = n33; te[14] = n34
+		te[3] = n41; te[7] = n42; te[11] = n43; te[15] = n44
 
-	// 	return this
-	// }
+		return this
+	}
 
-	// /**
-	//  * Resets this matrix to identity.
-	//  */
-	// identity(): Matrix4 {
-	// 	this.set(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
+	/**
+	 * Resets this matrix to identity.
+	 */
+	identity(): Matrix4 {
+		this.set(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
 
-	// 	return this
-	// }
+		return this
+	}
 
 	// clone(): Matrix4 {
 	// 	return new Matrix4().fromArray(this.elements)
