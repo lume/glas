9245192eac2598f694c721f00338f52cad31d0aa
Kara Rawson <rawsonkara@gmail.com>
2021-02-13 19:36:00 -0500

    Added work around for incorrect module path during AssemblyScript compilation.

--- src/as/test.spec.ts
+++ src/as/test.spec.ts
@@ -1,7 +1,10 @@
 import {num} from './test'
-// import {SHORT} from 'aswebglue/src/WebGL'
-import {SHORT} from 'aswebglue'
+// import {SHORT} from 'aswebglue' // FIXME: Broken in AssemblyScript
+import {SHORT} from '../../node_modules/aswebglue/src/webgl'
 
+/**
+ * tests our import of aswebglue by multiply WebGL.SHORT by two.
+ */
 describe('aswebglue', () => {
 	it('was imported', () => {
 		expect(num).toBe(SHORT * 2)
--- src/as/test.ts
+++ src/as/test.ts
@@ -1,4 +1,4 @@
-// import {SHORT} from 'aswebglue/src/WebGL'
-import {SHORT} from 'aswebglue'
+// import {SHORT} from 'aswebglue' // FIXME: Broken in AssemblyScript
+import {SHORT} from '../../node_modules/aswebglue/src/webgl'
 
 export const num: u32 = SHORT * 2
