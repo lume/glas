f30710d304e3d45551a8ceb55048c4eeaa4bf693
Joe Pea <joe@trusktr.io>
2021-02-21 23:31:20 -0800

    make glas just a lib, and the examples import it

--- src/as/index.spec.ts
+++ src/as/index.spec.ts
@@ -12,3 +12,12 @@ import './constants.spec'
 import './utils.spec'
 
 import './aswebglue.spec'
+
+import {Box3} from './index'
+
+describe('entrypoint', () => {
+	it('exports some stuff', () => {
+		const box = new Box3()
+		expect(box instanceof Box3).toBeTruthy()
+	})
+})
