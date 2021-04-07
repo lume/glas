47e885339a0b55a74e2b1a94b56b49849a026cfb
Joe Pea <joe@trusktr.io>
2021-04-06 23:44:33 -0700

    improve instrumentation to detect methods defined inline, f.e. `this.foo = function() {...}`

--- instrument.js
+++ instrument.js
@@ -10,7 +10,7 @@ function instrument(folder, pattern) {
 		for (const file of files) {
 			if (!file.match(pattern)) continue
 
-			~(async function() {
+			~(async function () {
 				let code = await fs.readFile(file, {encoding: 'utf8'})
 
 				// remove strict mode, which allows us to use arguments.callee below
@@ -19,16 +19,30 @@ function instrument(folder, pattern) {
 
 				// prettier-ignore
 				code = code.replace(fnHeader, match => `${match}
-					// get a list of all used classes
-					if (!window.usedClasses) window.usedClasses = new Set;
-					if (this && this.constructor) window.usedClasses.add(this.constructor.name);
+					const isObject = this && this.constructor
 
-					// get a list of all used methods
-					if (!window.usedMethodsPerClass) window.usedMethodsPerClass = new Map;
-					if (this && this.constructor) {
+					if (isObject) {
+						// get a list of all used classes
+						if (!window.usedClasses) window.usedClasses = new Set
+
+						window.usedClasses.add(this.constructor.name)
+
+						let propNames = window.propertyNames
+						if (!propNames) propNames = window.propertyNames = new WeakMap;
+
+						let names = propNames.get(this.constructor)
+						if (!names) propNames.set(this.constructor, names = [])
+						for (const prop in this) names.push(prop)
+
+						// get a list of all used methods
+						if (!window.usedMethodsPerClass) window.usedMethodsPerClass = new Map
+
 						let classMethods = window.usedMethodsPerClass.get(this.constructor.name)
 						if (!classMethods) window.usedMethodsPerClass.set(this.constructor.name, classMethods = new Set)
-						classMethods.add(arguments.callee.name);
+
+						let method = arguments.callee.name
+						if (method) classMethods.add(arguments.callee.name)
+						else if (names.some(name => this[method = name] === arguments.callee)) classMethods.add(method)
 					}
 				`)
 
