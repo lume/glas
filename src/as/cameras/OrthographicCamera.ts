4e6b4e38112503bcc0ace8983cb7b35214a69d2f
Joe Pea <joe@trusktr.io>
2022-06-26 11:57:29 -0700

    join doc comments with the members

--- src/as/cameras/OrthographicCamera.ts
+++ src/as/cameras/OrthographicCamera.ts
@@ -23,6 +23,13 @@ class OrthographicView {
  * @see <a href="https://github.com/mrdoob/three.js/blob/master/src/cameras/OrthographicCamera.js">src/cameras/OrthographicCamera.js</a>
  */
 export class OrthographicCamera extends Camera {
+	readonly isOrthographicCamera: boolean = true
+
+	type: string = 'OrthographicCamera'
+
+	zoom: f32 = 1
+	view: null | OrthographicView = null
+
 	/**
 	 * @param left Camera frustum left plane.
 	 * @param right Camera frustum right plane.
@@ -31,43 +38,6 @@ export class OrthographicCamera extends Camera {
 	 * @param near Camera frustum near plane.
 	 * @param far Camera frustum far plane.
 	 */
-
-	type: string = 'OrthographicCamera'
-
-	zoom: f32 = 1
-	view: null | OrthographicView = null
-	isOrthographicCamera: boolean = true
-
-	/**
-	 * Camera frustum left plane.
-	 */
-	//  left: f32
-
-	/**
-	 * Camera frustum right plane.
-	 */
-	//  right: f32
-
-	/**
-	 * Camera frustum top plane.
-	 */
-	//  top: f32
-
-	/**
-	 * Camera frustum bottom plane.
-	 */
-	//  bottom: f32
-
-	/**
-	 * Camera frustum near plane.
-	 */
-	//  near: f32
-
-	/**
-	 * Camera frustum far plane.
-	 */
-	//  far: f32
-
 	constructor(
 		public left: f32 = -1,
 		public right: f32 = 1,
@@ -78,34 +48,6 @@ export class OrthographicCamera extends Camera {
 	) {
 		super()
 		this.updateProjectionMatrix()
-	}
-
-	updateProjectionMatrix(): void {
-		const dx = (this.right - this.left) / (2 * this.zoom)
-		const dy = (this.top - this.bottom) / (2 * this.zoom)
-		const cx = (this.right + this.left) / 2
-		const cy = (this.top + this.bottom) / 2
-
-		let left = cx - dx
-		let right = cx + dx
-		let top = cy + dy
-		let bottom = cy - dy
-
-		if (this.view !== null && this.view.enabled) {
-			const zoomW = this.zoom / (this.view.width / this.view.fullWidth)
-			const zoomH = this.zoom / (this.view.height / this.view.fullHeight)
-			const scaleW = (this.right - this.left) / this.view.width
-			const scaleH = (this.top - this.bottom) / this.view.height
-
-			left += scaleW * (this.view.offsetX / zoomW)
-			right = left + scaleW * (this.view.width / zoomW)
-			top -= scaleH * (this.view.offsetY / zoomH)
-			bottom = top - scaleH * (this.view.height / zoomH)
-		}
-
-		this.projectionMatrix.makeOrthographic(left, right, top, bottom, this.near, this.far)
-
-		this.projectionMatrixInverse.getInverse(this.projectionMatrix)
 	}
 
 	setViewOffset(fullWidth: f32, fullHeight: f32, offsetX: f32, offsetY: f32, width: f32, height: f32): void {
@@ -140,6 +82,37 @@ export class OrthographicCamera extends Camera {
 		this.updateProjectionMatrix()
 	}
 
+	/**
+	 * Updates the camera projection matrix. Must be called after change of parameters.
+	 */
+	updateProjectionMatrix(): void {
+		const dx = (this.right - this.left) / (2 * this.zoom)
+		const dy = (this.top - this.bottom) / (2 * this.zoom)
+		const cx = (this.right + this.left) / 2
+		const cy = (this.top + this.bottom) / 2
+
+		let left = cx - dx
+		let right = cx + dx
+		let top = cy + dy
+		let bottom = cy - dy
+
+		if (this.view !== null && this.view.enabled) {
+			const zoomW = this.zoom / (this.view.width / this.view.fullWidth)
+			const zoomH = this.zoom / (this.view.height / this.view.fullHeight)
+			const scaleW = (this.right - this.left) / this.view.width
+			const scaleH = (this.top - this.bottom) / this.view.height
+
+			left += scaleW * (this.view.offsetX / zoomW)
+			right = left + scaleW * (this.view.width / zoomW)
+			top -= scaleH * (this.view.offsetY / zoomH)
+			bottom = top - scaleH * (this.view.height / zoomH)
+		}
+
+		this.projectionMatrix.makeOrthographic(left, right, top, bottom, this.near, this.far)
+
+		this.projectionMatrixInverse.getInverse(this.projectionMatrix)
+	}
+
 	// toJSON(meta) {
 	// 	const data = Object3D.prototype.toJSON.call(this, meta)
 
@@ -155,12 +128,4 @@ export class OrthographicCamera extends Camera {
 
 	// 	return data
 	// }
-
-	/**
-	 * Updates the camera projection matrix. Must be called after change of parameters.
-	 */
-	// updateProjectionMatrix(): void
-	// setViewOffset(fullWidth: f32, fullHeight: f32, offsetX: f32, offsetY: f32, width: f32, height: f32): void
-	// clearViewOffset(): void
-	// toJSON(meta?: any): any
 }
